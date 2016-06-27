f20a3ac55b31a63d2373ea99e6229c931117dbcb
Michael Sromin <michaelsromin@gmail.com>
2016-06-27 14:45:06 +0300

    the multiple login file added

---
+++ client/js/modules/userLoginApp.js
@@ -0,0 +1,61 @@
+(function(){
+    'use strict';
+    angular
+        .module('userLiginApp', [])
+        .controller('userLoginFormCtrl', ['$scope','$http', '$window', function($scope, $http, $window){
+            
+            $scope.dangerMessage = "";
+            
+            $scope.studentLogin = function(){
+                console.log('logging in as a student..');
+                var data = {
+                    username: $scope.username,
+                    password: $scope.password,
+                    role: "student"
+                };
+                $http({
+                    method:'POST',
+                    url: '/auth/login',
+                    data: data
+                })
+                    .success(function(res){
+                        console.log(res);
+                        if (res.message === 'logged-in'){
+                            $window.location.href = '/discussions';
+                        }
+                        else{
+                            $scope.dangerMessage = res.message;
+                        }
+                    })
+                    .error(function(err, status){
+                        console.log(err.statusText);
+                    });
+            }
+
+            $scope.instructorLogin = function(){
+                console.log('logging in as an instructor..');
+                var data = {
+                    username: $scope.username,
+                    password: $scope.password,
+                    role: "instructor"
+                };
+                $http({
+                    method:'POST',
+                    url: '/auth/login',
+                    data: data
+                })
+                    .success(function(res){
+                        console.log(res);
+                        if (res.message === 'logged-in'){
+                            $window.location.href = '/discussions';
+                        }
+                        else{
+                            $scope.dangerMessage = res.message;
+                        }
+                    })
+                    .error(function(err, status){
+                        console.log(err.statusText);
+                    });
+            }
+        }]);
+})();
